d962edce34150ad22aa36c2ae60e13aa213cbd31
Cyan <snow22314@outlook.com>
2022-04-08 08:17:18 +0800

    fix

--- file-toolkit-test-web/src/main/java/io/github/nichetoolkit/file/FileToolkitTestWebApplication.java
+++ file-toolkit-test-web/src/main/java/io/github/nichetoolkit/file/FileToolkitTestWebApplication.java
@@ -2,12 +2,21 @@ package io.github.nichetoolkit.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class FileToolkitTestWebApplication {
+@ComponentScan(basePackages = "io.github.nichetoolkit")
+public class FileToolkitTestWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FileToolkitTestWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FileToolkitTestWebApplication.class);
     }
 
 }
