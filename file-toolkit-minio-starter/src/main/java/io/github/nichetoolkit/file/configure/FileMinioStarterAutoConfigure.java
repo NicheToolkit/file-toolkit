79f66a052aa82e2e1218e8fb122ae8a242abcfe5
Cyan <snow22314@outlook.com>
2022-06-17 17:30:15 +0800

    拆分minio utils

--- file-toolkit-minio-starter/src/main/java/io/github/nichetoolkit/file/configure/FileMinioStarterAutoConfigure.java
+++ file-toolkit-minio-starter/src/main/java/io/github/nichetoolkit/file/configure/FileMinioStarterAutoConfigure.java
@@ -1,11 +1,7 @@
 package io.github.nichetoolkit.file.configure;
 
-import io.github.nichetoolkit.file.minio.MinioUtils;
-import io.github.nichetoolkit.file.service.AsyncFileService;
-import io.github.nichetoolkit.file.service.impl.MinioFileService;
 import io.github.nichetoolkit.file.video.MinioHttpRequestHandler;
 import io.github.nichetoolkit.file.video.VideoHttpRequestHandler;
-import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
- * <p>FileServiceAutoConfigure</p>
+ * <p>FileMinioStarterAutoConfigure</p>
  * @author Cyan (snow22314@outlook.com)
  * @version v1.0.0
  */
@@ -23,22 +19,9 @@ import tk.mybatis.spring.annotation.MapperScan;
 @org.mybatis.spring.annotation.MapperScan("io.github.nichetoolkit.file.mapper")
 @MapperScan("io.github.nichetoolkit.file.mapper")
 @ComponentScan(basePackages = {"io.github.nichetoolkit.file"})
-public class FileMinioAutoConfigure {
-
-    public FileMinioAutoConfigure() {
-        log.debug("================= file-minio-auto-config initiated ！ ===================");
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(value = MinioClient.class)
-    public MinioClient minioClient(FileMinioProperties minioProperties) {
-        return MinioUtils.createMinioClient(minioProperties);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(value = MinioUtils.class)
-    public MinioUtils minioUtils(MinioClient minioClient, FileMinioProperties minioProperties) {
-        return new MinioUtils(minioClient, minioProperties);
+public class FileMinioStarterAutoConfigure {
+    public FileMinioStarterAutoConfigure() {
+        log.debug("================= file-minio-starter-auto-config initiated ！ ===================");
     }
 
     @Bean
